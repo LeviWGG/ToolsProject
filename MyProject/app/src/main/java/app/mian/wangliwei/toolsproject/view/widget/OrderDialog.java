0ad7257f2405f2bbc8e10608b8e72f7f88477160
LeviWGG <yugiop@126.com>
2018-03-26 18:53:00 +0800

    modify dialog to adaptation

--- MyProject/app/src/main/java/app/mian/wangliwei/toolsproject/view/widget/OrderDialog.java
+++ MyProject/app/src/main/java/app/mian/wangliwei/toolsproject/view/widget/OrderDialog.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -55,7 +57,12 @@ public class OrderDialog extends BitkerDialog {
     }
 
     @Override
-    public void initView() {}
+    public void initView() {
+        Display display = getWindow().getWindowManager().getDefaultDisplay();
+        WindowManager.LayoutParams lp = getWindow().getAttributes();
+        lp.width = (int)(display.getWidth() * 0.8);
+        getWindow().setAttributes(lp);
+    }
 
     @Override
     public int getLayoutId() {
